4db2568ebed9530e72ba109f15a67097cc142403
dayeon <hidayeonee@gmail.com>
2024-12-17 18:00:19 +0900

    board_Study 생성

---
+++ src/main/java/com/example/board/DTO/BoardDTO.java
@@ -0,0 +1,30 @@
+package com.example.board.DTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString //테이블 join을 사용할 때는 2개 Entity 중에 하나에는 toString이 없어야 함
+@Builder
+//@Data
+public class BoardDTO {
+    //Entity에 있는 변수들(사용할 것)만 복사
+    //공통 삽입, 수정, 목록, 상세보기
+    //DTO로 삽입, 수정, 목록 등 개별적으로 작업
+    private Integer id;
+    private String subject;
+    private String content;
+    private String author;
+    private LocalDateTime regdate;
+    private LocalDateTime modDate;
+}
